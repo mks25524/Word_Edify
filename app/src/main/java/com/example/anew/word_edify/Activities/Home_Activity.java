551e0370df3a3e560f9dbea507fc9c42c8ed07ac
mks25524 <mks25524withme@gmail.com>
2017-03-20 22:27:04 +0600

    complete HomeActivity With button

--- app/src/main/java/com/example/anew/word_edify/Activities/Home_Activity.java
+++ app/src/main/java/com/example/anew/word_edify/Activities/Home_Activity.java
@@ -1,15 +1,42 @@
 package com.example.anew.word_edify.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.anew.word_edify.R;
 
 public class Home_Activity extends AppCompatActivity {
 
+    private Button buttonPlay;
+    private Button buttonHowToPlay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_ );
+        setContentView(R.layout.activity_home);
+        buttonPlay=(Button)findViewById(R.id.playBtn);
+        buttonHowToPlay=(Button)findViewById(R.id.HowplayBtn);
+        // play button
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToGameBoard=new Intent(Home_Activity.this,Gameboard_Activity.class);
+                Home_Activity.this.startActivity(goToGameBoard);
+            }
+        });
+
+
+
+        //How to play button
+        buttonHowToPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToGameBoard=new Intent(Home_Activity.this,PlaHow_Activity.class);
+                Home_Activity.this.startActivity(goToGameBoard);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/anew/word_edify/Activities/PlaHow_Activity.java
@@ -0,0 +1,15 @@
+package com.example.anew.word_edify.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.anew.word_edify.R;
+
+public class PlaHow_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pla_how_);
+    }
+}
